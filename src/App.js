90ddeeac6cb498930dd8f5ccb8e127918abc8ec3
Sohyewon <sho628@naver.com>
2023-08-02 21:03:55 +0900

    우선 chat GPT가 짜준 그대로 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const MyComponent = () => {
+  const [responseData, setResponseData] = useState(null);
+
+  useEffect(() => {
+    // API 호출을 수행하는 함수
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://15.164.253.191:3000/auth');
+
+        // 응답 데이터를 responseData state에 저장
+        setResponseData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // responseData에 데이터가 있을 경우 출력
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {responseData ? (
+        <pre>{JSON.stringify(responseData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
