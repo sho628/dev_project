3d82f59a16d4159b10bde064240406c611c79076
root <root@ip-172-31-2-1.ap-northeast-2.compute.internal>
2023-08-03 02:43:13 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const MyComponent = () => {
-  const [responseData, setResponseData] = useState(null);
+function App() {
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 19.99,
+      description: 'This is the description for Product 1.',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 29.99,
+      description: 'This is the description for Product 2.',
+    },
+    // Add more products here
+  ];
 
-  useEffect(() => {
-    // API 호출을 수행하는 함수
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://15.164.253.191:3000/auth');
-
-        // 응답 데이터를 responseData state에 저장
-        setResponseData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  // responseData에 데이터가 있을 경우 출력
   return (
-    <div>
-      {responseData ? (
-        <pre>{JSON.stringify(responseData, null, 2)}</pre>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="App">
+      <header>
+        <h1>Welcome to Our E-commerce Store</h1>
+      </header>
+      <main>
+        <section className="products">
+          <h2>Products</h2>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>Price: ${product.price}</p>
+                <button>Add to Cart</button>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} E-commerce Store. All rights reserved.</p>
+      </footer>
     </div>
   );
-};
+}
 
-export default MyComponent;
+export default App;
